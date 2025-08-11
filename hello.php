d0475af6414c4bf03884909c7eae855ff3cf3536
James Casquejo <145674250+xFakePeke@users.noreply.github.com>
2025-08-11 17:27:27 +0800

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP</title>
+</head>
+<body>
+    <?php
+        $name = 'James Casquejo';
+        echo "<p> Hello ".$name;
+    ?>
+</body>
+</html>
